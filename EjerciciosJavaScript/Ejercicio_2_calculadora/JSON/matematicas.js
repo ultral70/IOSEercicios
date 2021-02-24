d08c43ae52ddbb54aa0b4e2c22478f3a2e2cc210
ultral70 <40667722+ultral70@users.noreply.github.com>
2021-02-24 21:47:14 +0100

    Add files via upload

---
+++ EjerciciosJavaScript/Ejercicio_2_calculadora/JSON/matematicas.js
@@ -0,0 +1,25 @@
+
+/*
+La funcion suma tiene 2 parametros de entrada y uno de salida con el 
+resultado
+*/
+
+function sumar(a,b){
+  var resultado = a + b;
+  return resultado;
+}
+
+function restar(a,b){
+  var resultado = a - b;
+  return resultado;
+}
+
+function dividir(a,b){
+  var resultado = a / b;
+  return resultado;
+}
+
+function multiplicar(a,b){
+  var resultado = a * b;
+  return resultado;
+}
